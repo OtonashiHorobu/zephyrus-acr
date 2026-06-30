d5c29fa82fec7e337a0adad2c8f3c8c26097481b
OtonashiHorobu <3164432713@qq.com>
2026-07-01 00:21:00 +0800

    build(cmake): fix libpng build

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -16,10 +16,11 @@ function(setup_dependencies)
         URI "gh:madler/zlib@1.3.2#da607da739fa6047df13e66a2af6b8bec7c2a498"
         OVERRIDE_FIND_PACKAGE
         OPTIONS
-            "ZLIB_BUILD_TESTING OFF"
-            "ZLIB_BUILD_SHARED OFF"
+        "ZLIB_BUILD_TESTING OFF"
+        "ZLIB_BUILD_SHARED OFF"
     )
     add_library(ZLIB::ZLIB ALIAS zlibstatic)
+    set(ZLIB_INCLUDE_DIRS "${zlib_SOURCE_DIR}" "${zlib_BINARY_DIR}")
 
     CPMAddPackage(
         URI "https://gitlab.com/bzip2/bzip2.git@1.0.8#6a8690fc8d26c815e798c588f796eabe9d684cf0"
@@ -44,7 +45,7 @@ function(setup_dependencies)
         URI "gh:google/brotli@1.2.0#028fb5a23661f123017c060daa546b55cf4bde29"
         OVERRIDE_FIND_PACKAGE
         OPTIONS
-            "BROTLI_DISABLE_TESTS ON"
+        "BROTLI_DISABLE_TESTS ON"
     )
     set(BROTLIDEC_LIBRARIES brotlidec CACHE INTERNAL "Force freetype to use our brotlidec" FORCE)
     set(BROTLIDEC_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/_deps/brotli-src/c/include" CACHE INTERNAL "Force freetype to use our brotlidec" FORCE)
@@ -53,21 +54,21 @@ function(setup_dependencies)
         URI "gh:pnggroup/libpng@1.8.0#d1d0abeffede1cc898ddc3d0e600839cf026d749"
         OVERRIDE_FIND_PACKAGE
         OPTIONS
-            "PNG_TESTS OFF"
-            "PNG_TOOLS OFF"
-            "PNG_SHARED OFF"
-            "SKIP_INSTALL_ALL ON"
+        "PNG_TESTS OFF"
+        "PNG_TOOLS OFF"
+        "PNG_SHARED OFF"
+        "SKIP_INSTALL_ALL ON"
     )
-    set(PNG_LIBRARY "${PROJECT_BINARY_DIR}/_deps/libpng-src")
-    set(PNG_PNG_INCLUDE_DIR "${PROJECT_BINARY_DIR}/_deps/libpng-src")
+    set(PNG_LIBRARY "${libpng_SOURCE_DIR}")
+    set(PNG_PNG_INCLUDE_DIR "${libpng_SOURCE_DIR}")
     add_library(PNG::PNG ALIAS png_static)
 
     CPMAddPackage(
         URI "gh:harfbuzz/harfbuzz@14.2.1#56feae4035bdd48f62ba2b8d8c16232d4d89b3a4"
         OVERRIDE_FIND_PACKAGE
         OPTIONS
-            "HB_HAVE_FREETYPE OFF"
-            "SKIP_INSTALL_ALL ON"
+        "HB_HAVE_FREETYPE OFF"
+        "SKIP_INSTALL_ALL ON"
     )
     add_library(HarfBuzz::HarfBuzz ALIAS harfbuzz)
 
@@ -75,13 +76,13 @@ function(setup_dependencies)
         URI "gh:freetype/freetype@2.14.3#0a0221a1347e2f1e07c395263540026e9a0aa7c7"
         OVERRIDE_FIND_PACKAGE
         OPTIONS
-            "FT_DYNAMIC_HARFBUZZ FALSE"
-            "FT_REQUIRE_ZLIB ON"
-            "FT_REQUIRE_PNG ON"
-            "FT_REQUIRE_BZIP2 ON"
-            "FT_REQUIRE_HARFBUZZ ON"
-            "FT_REQUIRE_BROTLI ON"
-            "SKIP_INSTALL_ALL ON"
+        "FT_DYNAMIC_HARFBUZZ FALSE"
+        "FT_REQUIRE_ZLIB ON"
+        "FT_REQUIRE_PNG ON"
+        "FT_REQUIRE_BZIP2 ON"
+        "FT_REQUIRE_HARFBUZZ ON"
+        "FT_REQUIRE_BROTLI ON"
+        "SKIP_INSTALL_ALL ON"
     )
     add_library(Freetype::Freetype ALIAS freetype)
     CPMAddPackage(
